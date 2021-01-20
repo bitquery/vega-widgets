8f1fe84a9adb54dc0c4b06389bc3070c86ee10b9
Spendil <spendil@mail.ru>
2021-01-20 21:16:01 +0300

    update config & README

---
+++ README.md
@@ -0,0 +1,12 @@
+# graphs
+Graph widgets library based on vega
+# Description
+Library for graph visualization based on blockchain data
+
+# Quick start
+Several quick start options are available:
+* Install with npm `npm i @bitquery/graph`
+* Clone the repo: `git clone https://github.com/bitquery/vega-wwidgets.git`
+* Download the latest release
+# License
+This project is licensed under the terms of the MIT license.
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,64 @@
 const path = require('path');
+const webpack = require('webpack')
+
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
-  entry: './index.js',
-  output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'index.js',
-    libraryTarget: "umd",
-    library: "vega-widgets"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src'),
-        use: {
-          loader: 'babel-loader'
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  externals: {
-    'react': 'commonjs react' 
-  }
+	entry: './index.js',
+	output: {
+		path: path.join(__dirname, '/dist'),
+		filename: 'index.js',
+		libraryTarget: "umd",
+		library: "vega-widgets"
+	},
+	externals: {
+			react: {
+				root: "React",
+				commonjs2: "react",
+				commonjs: "react",
+				amd: "react",
+				umd: "react",
+			},
+			"react-dom": {
+				root: "ReactDOM",
+				commonjs2: "react-dom",
+				commonjs: "react-dom",
+				amd: "react-dom",
+				umd: "react-dom",
+		}
+	},
+	plugins: [
+		new webpack.ProgressPlugin(),
+	],
+	module: {
+		rules: [
+		{
+			test: /\.js$/,
+			include: path.resolve(__dirname, 'src'),
+			use: {
+			loader: 'babel-loader'
+			},
+		},
+		{
+			test: /\.css$/,
+			use: ['style-loader', 'css-loader']
+		}
+		]
+	},
+	optimization: {
+		minimize: true,
+		minimizer: [
+		new TerserPlugin({
+			parallel: true,
+			cache: true,
+			sourceMap: true,
+			terserOptions: {
+			format: {
+				comments: false,
+			},
+			},
+			extractComments: false,
+		})
+		],
+	},
 };
