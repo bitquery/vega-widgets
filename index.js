ad199b4752d97379dc7f1a2a2b597fb6fc6c0407
Spendil <spendil@mail.ru>
2021-03-23 14:30:34 +0300

    rework 2.0

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 import BarWidgetEditor from './src/components/widgets/BarWidgetEditor'
-import BarWidgetRenderer from './src/components/widgets/BarWidgetRenderer'
+import barWidgetRenderer from './src/components/widgets/barWidgetRenderer'
 import PieWidgetEditor from './src/components/widgets/PieWidgetEditor'
-import PieWidgetRenderer from './src/components/widgets/PieWidgetRenderer'
+import pieWidgetRenderer from './src/components/widgets/pieWidgetRenderer'
 
-class BarPlugin {
+class Plugin {
 	constructor() {
+		this.dependencies = [
+			'https://cdn.jsdelivr.net/npm/vega@5.19.1',
+			'https://cdn.jsdelivr.net/npm/vega-lite@5.0.0',
+			'https://cdn.jsdelivr.net/npm/vega-embed@6.15.1'
+		]
+	}
+}
+
+class BarPlugin extends Plugin {
+	constructor() {
+		super()
 		this.id = 'vega.bar'
 		this.name = 'Bar Widget'
 		this.editor = BarWidgetEditor
-		this.renderer = BarWidgetRenderer
+		this.renderer = barWidgetRenderer
+		this.source = 'node_modules/vega-widgets/src/components/widgets/barWidgetRenderer.js'
 	}
 	supportsModel(model) {
 		for (let key in model) {
@@ -17,12 +29,14 @@ class BarPlugin {
 		}
 	}
 }
-class PiePlugin {
+class PiePlugin extends Plugin {
 	constructor() {
+		super()
 		this.id = 'vega.pie'
 		this.name = 'Pie Widget'
 		this.editor = PieWidgetEditor
-		this.renderer = PieWidgetRenderer
+		this.renderer = pieWidgetRenderer
+		this.source = 'node_modules/vega-widgets/src/components/widgets/pieWidgetRenderer.js'
 	}
 	supportsModel(model) {
 		for (let key in model) {
---
+++ src/components/widgets/pieWidgetRenderer.js
@@ -0,0 +1,37 @@
+import vega from 'vega'
+import vegaLite from 'vega-lite'
+import vegaEmbed from 'vega-embed'
+export default async function pieWidgetRenderer(ds, config, el) {
+	let values = undefined
+	if (!ds.values) {
+		const data = await ds.fetcher()
+		const json = await data.json()
+		values = ds.setupData(json)
+	} else {
+		values = ds.values
+	}
+	let cfg = {
+		$schema: 'https://vega.github.io/schema/vega-lite/v4.json',
+		description: 'A simple bar chart with embedded data.',
+		width: 'container',
+		height: 'container',
+		mark: {"type": "arc", "tooltip": true},
+		selection: {
+			highlight: {
+				type: "single",
+				empty: "none",
+				on: "mouseover"
+			}
+		},
+		...config,
+		data: {
+			values
+		},
+		view: {stroke: null}
+	}
+	try {
+		el && vegaEmbed(`#${el}`, cfg)
+	} catch (error) {
+		console.log(error)
+	}
+}
